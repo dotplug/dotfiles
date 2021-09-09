697bd7b8381a7c1f25832da1551c11c782e9387a
César Alberca <cesalberca@gmail.com>
2021-09-09 13:02:49 +0200

    Update oh my zsh installation url

--- zsh/oh-my-zsh/install.sh
+++ zsh/oh-my-zsh/install.sh
@@ -1,3 +1,3 @@
 if [[ ! -d ~/.oh-my-zsh ]]; then
-    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
