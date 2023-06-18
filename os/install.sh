25527c235bb2ce321aa0f1a446b8da4ea62cf622
Juanan <jaocarvajal@gmail.com>
2023-06-18 22:07:18 +0200

    feat: update prompt message for the os/install process to just show the process without the normal output

--- os/install.sh
+++ os/install.sh
@@ -1,4 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
 
 # ============================================================================
 # FUNCTIONS
@@ -11,18 +15,18 @@
 install_homebrew() {
     if test ! $(which brew)
     then
-        blue "Installing Homebrew"
+        blue "[OS] Installing Homebrew"
 
         # Install the correct homebrew for each OS type
         if test "$(uname)" = "Darwin"
         then
-            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" 1>/dev/null
         elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
         then
-           ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
+           ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)" 1>/dev/null
         fi
     else
-        green "Homebrew is already installed"
+        green "[OS] Homebrew is already installed"
     fi
 
     # If Homebrew is installed under /opt/homebrew folder
@@ -37,13 +41,13 @@ function generate_brewfiles() {
     local SOURCE_FOLDER="$DOTFILES_DIR"
     local DESTINATION_FILE="$HOME/.Brewfile"
 
-    blue "Cleanup $DESTINATION_FILE"
+    blue "[OS] Cleanup $DESTINATION_FILE"
     rm $DESTINATION_FILE
 
-    blue "Search for $SOURCE_FILE inside $SOURCE_FOLDER"
+    blue "[OS] Search for $SOURCE_FILE inside $SOURCE_FOLDER and generate a merged Brewfile"
     cat $(find -H "$SOURCE_FOLDER" -type f -name "$SOURCE_FILE") >> $DESTINATION_FILE
 
-    green "Generated $DESTINATION_FILE"
+    green "[OS] Generated $DESTINATION_FILE"
 }
 
 # The Brewfile handles Homebrew-based app and library installs, but there may
@@ -51,16 +55,17 @@ function generate_brewfiles() {
 # command line interface to it that we can use to just install everything, so
 # yeah, let's do that.
 update_mac_apps_and_libraries() {
-    local update_command="sudo softwareupdate -i -r"
-    blue "› $update_command"
-    $update_command
+    blue "[OS] Update Mac App Store apps"
+    sudo /usr/sbin/softwareupdate -i -r 1>/dev/null
+    green "[OS] Updated!"
 }
 
 # Install Rosetta
 install_rosetta() {
     if [[ `uname -m` == 'arm64' ]]; then
-        blue "Install Rosetta"
-        /usr/sbin/softwareupdate --install-rosetta --agree-to-license
+        blue "[OS] Install Rosetta"
+        sudo /usr/sbin/softwareupdate --install-rosetta --agree-to-license 1>/dev/null
+        green "[OS] Rosetta installed!"
     fi
 }
 
@@ -70,35 +75,42 @@ install_rosetta() {
 
 if test "$(uname)" = "Darwin"
 then
-    blue "Install Homebrew"
+    blue "[OS] Request sudo password to future process"
+    sudo echo "Password added!"
+
+    blue "[OS] Install Homebrew"
     install_homebrew
     generate_brewfiles
 
-    blue "Go to $HOME directory"
+    blue "[OS] Go to $HOME directory"
     cd $HOME
 
-    blue "Install Brew apps defined in the Brewfile"
-    brew bundle --cleanup --global
+    blue "[OS] Install Brew apps defined in the Brewfile (Takes a lot of time first time to install everything)"
+    brew bundle --cleanup --global 1>/dev/null
+    green "[OS] Installed!"
 
-    blue "Update all the apps defined in the Brewfile"
-    brew update
+    blue "[OS] Update all the apps defined in the Brewfile"
+    brew update 1>/dev/null
+    green "[OS] Updated!"
 
-    blue "Upgrade all the cask apps"
+    blue "[OS] Upgrade all the cask apps"
     brew upgrade
+    green "[OS] Upgraded!"
 
-    blue "Clean old stuff dont required"
+    blue "[OS] Clean old stuff dont required"
     brew cleanup
+    green "[OS] Cleaned!"
 
-    blue "Pass Doctor to check everything is fine"
+    blue "[OS] Pass Doctor to check everything is fine"
     brew doctor
+    green "[OS] Everything OK!"
 
-    blue "Update Mac App Store apps"
     update_mac_apps_and_libraries
 
     install_rosetta
 
-    blue "Return to the last directory"
+    blue "[OS] Return to the last directory"
     cd -
 
-    green "Successful installation"
+    green "[OS] Successful installation"
 fi
